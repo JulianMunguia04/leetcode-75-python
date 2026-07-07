e4899a0dc232f832f894f6af691876cda03e47be
JulianMunguia04 <julianmunguia04@gmail.com>
2026-07-06 21:39:47 -0600

    Folder Structure

---
+++ array_string/greatest_common_divisor_of_strings.py
@@ -0,0 +1,11 @@
+#Notes
+
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+
+#Time Complexity O(n), n being the count of numbers in nums. ie Linear Time

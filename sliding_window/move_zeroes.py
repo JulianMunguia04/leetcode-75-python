71f62c25cb724610c70ac49009ddc6a2747edfce
Julian Munguia <julianmunguia04@gmail.com>
2026-07-08 22:44:50 -0600

    Create move_zeroes.py

---
+++ sliding_window/move_zeroes.py
@@ -0,0 +1,8 @@
+#Leetcode 283 — Easy
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """

4a660dcc79db43ec5527b8a909cb5f67baee84b1
Julian Munguia <julianmunguia04@gmail.com>
2026-07-11 11:40:56 +0900

    Update maximum_average_subarray_one.py

--- sliding_window/maximum_average_subarray_one.py
+++ sliding_window/maximum_average_subarray_one.py
@@ -1,2 +1,11 @@
 # Leetcode 643 Sliding Window
 
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        
+# Time Complexity

d3f3f0c7e0f7bd8cb2a4c3c4780e6661ed4fc5fd
Julian Munguia <julianmunguia04@gmail.com>
2026-07-12 20:47:01 +0900

    Update find_the_highest_altitude.py

--- prefix_sum/find_the_highest_altitude.py
+++ prefix_sum/find_the_highest_altitude.py
@@ -1,1 +1,18 @@
-# 1732 Highesr Altitude Sum, prefix sum
+# 1732 Highest Altitude Sum, prefix sum
+
+class Solution(object):
+    def largestAltitude(self, gain):
+        """
+        :type gain: List[int]
+        :rtype: int
+        """
+        altitude = 0
+        highest = 0
+        for height in gain:
+            altitude += height
+            if altitude > highest:
+                highest = altitude
+        
+        return highest
+        
+# Time Complexity O(n)

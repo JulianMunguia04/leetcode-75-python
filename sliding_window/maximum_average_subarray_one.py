f8fb6ddb689ee03578bef4b1ed876c6f0d1a0153
Julian Munguia <julianmunguia04@gmail.com>
2026-07-12 12:27:05 +0900

    Update maximum_average_subarray_one.py

--- sliding_window/maximum_average_subarray_one.py
+++ sliding_window/maximum_average_subarray_one.py
@@ -2,10 +2,21 @@
 
 class Solution(object):
     def findMaxAverage(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: float
-        """
+        left = 0
+        total = 0
+        largest = float("-inf")
+
+        for right in range(len(nums)):
+            total += nums[right]
+
+            if right + 1 >= k:
+                if total > largest:
+                    largest = total
+
+                total -= nums[left]
+                left += 1
+
+        return largest / float(k)
+
         
 # Time Complexity

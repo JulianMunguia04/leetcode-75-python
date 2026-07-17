002f06643d480943385c3d0c89f258d719b31b41
Julian Munguia <julianmunguia04@gmail.com>
2026-07-17 16:14:07 +0900

    Update kids_with_the_greatest_number_of_candies.py

--- array_string/kids_with_the_greatest_number_of_candies.py
+++ array_string/kids_with_the_greatest_number_of_candies.py
@@ -1,1 +1,11 @@
 # 1431 Kids with the greatest number of candies. Array / String
+
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        
+# Time Complexity O(n)

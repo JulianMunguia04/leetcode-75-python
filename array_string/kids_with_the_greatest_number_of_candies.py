e1513705e67eb5f7714731c190d26daafdbe09ec
Julian Munguia <julianmunguia04@gmail.com>
2026-07-17 16:26:05 +0900

    Update kids_with_the_greatest_number_of_candies.py

--- array_string/kids_with_the_greatest_number_of_candies.py
+++ array_string/kids_with_the_greatest_number_of_candies.py
@@ -8,4 +8,20 @@ class Solution(object):
         :rtype: List[bool]
         """
         
+        most = 0
+        most_candies = []
+        
+        for i in range(len(candies)):
+            if candies[i] > most:
+                most = candies[i]
+                
+        for kid in candies:
+            if kid + extraCandies >= most:
+                most_candies.append(True)
+            else:
+                most_candies.append(False)
+                
+        return most_candies
+            
+        
 # Time Complexity O(n)

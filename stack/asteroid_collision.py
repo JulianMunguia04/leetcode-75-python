5fc2ef35e2477baa591a5afa02018a9d3153bd34
Julian Munguia <julianmunguia04@gmail.com>
2026-07-22 17:48:32 +0900

    Update asteroid_collision.py

--- stack/asteroid_collision.py
+++ stack/asteroid_collision.py
@@ -7,6 +7,21 @@ class Solution(object):
         :rtype: List[int]
         """
         
+        stack = []
         
+        for a in asteroids:
+            while stack and a < 0 and stack[-1] > 0:
+                if abs(a) > stack[-1]:
+                    stack.pop()
+                    continue
+                elif abs(a) == stack[-1]:
+                    stack.pop()
+                    break
+                elif abs(a) < stack[-1]:
+                    break
+                
+            else:
+                stack.append(a)
+        return stack
         
 # Time Complexity O(n)

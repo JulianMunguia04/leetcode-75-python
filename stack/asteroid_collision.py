afc5b632f4e067754e4822d7425ee83b2a33f325
Julian Munguia <julianmunguia04@gmail.com>
2026-07-22 16:30:34 +0900

    Update asteroid_collision.py

--- stack/asteroid_collision.py
+++ stack/asteroid_collision.py
@@ -1,2 +1,12 @@
 # Leetcode 735 Asteroid Collision, Stack
 
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        
+        
+        
+# Time Complexity O(n)

60f0f295720f4834e6e69eee36fd09d26fb8290d
Julian Munguia <julianmunguia04@gmail.com>
2026-07-23 21:26:35 +0900

    Update decode_string.py

--- stack/decode_string.py
+++ stack/decode_string.py
@@ -1,1 +1,30 @@
 # 394 Decode String, Stack
+
+class Solution(object):
+    def decodeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []
+        
+        for char in s:
+            if char != "]":
+                stack.append(char)
+            else:
+                substring = ""
+                while stack and stack[-1] != "[":
+                    substring = stack.pop() + substring
+                
+                stack.pop() 
+                
+                k = ""
+                while stack and stack[-1].isdigit():
+                    k = stack.pop() + k
+                
+                decoded_segment = int(k) * substring
+                stack.append(decoded_segment)
+                
+        return "".join(stack)
+
+# Time Complexity O(n)

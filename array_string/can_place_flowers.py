4cd0e44c39fbc4b2d9d7efb46097c09d6d986959
Julian Munguia <julianmunguia04@gmail.com>
2026-07-24 13:55:26 +0900

    Update can_place_flowers.py

--- array_string/can_place_flowers.py
+++ array_string/can_place_flowers.py
@@ -7,3 +7,17 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+
+        flowers = 0
+        length = len(flowerbed)
+        
+        for i in range(len(flowerbed)):
+            if not flowerbed[i] == 1:
+                if i == 0 or flowerbed[i-1] == 0:
+                    if i == length - 1 or flowerbed[i+1] == 0:
+                        flowerbed[i] = 1
+                        flowers += 1
+        if flowers >= n:
+            return True
+
+        return False

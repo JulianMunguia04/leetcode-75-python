5b178175b9313788c09d0aeb2b46537e09a89002
Julian Munguia <julianmunguia04@gmail.com>
2026-07-24 13:55:06 +0900

    Update can_place_flowers.py

--- array_string/can_place_flowers.py
+++ array_string/can_place_flowers.py
@@ -1,2 +1,9 @@
 # 605 Can Place Flowers, array / string
 
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """

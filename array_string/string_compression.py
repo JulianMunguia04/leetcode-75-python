1aa7bae9b76ae00523d932ee0f7350557f9da5c1
Julian Munguia <julianmunguia04@gmail.com>
2026-07-27 10:13:39 +0900

    Update string_compression.py

--- array_string/string_compression.py
+++ array_string/string_compression.py
@@ -1,1 +1,9 @@
 # 443 String Compression, String / Array
+
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        

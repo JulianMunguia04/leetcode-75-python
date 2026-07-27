5e263acf8853c9a55bb4d0392d085a755a70117c
Julian Munguia <julianmunguia04@gmail.com>
2026-07-27 16:22:43 +0900

    Update string_compression.py

--- array_string/string_compression.py
+++ array_string/string_compression.py
@@ -1,9 +1,31 @@
 # 443 String Compression, String / Array
 
-class Solution(object):
+cclass Solution(object):
     def compress(self, chars):
         """
         :type chars: List[str]
         :rtype: int
         """
+
+        read = 0
+        write = 0
+
+        while read < len(chars):
+            char = chars[read]
+            count = 0
+
+            while read < len(chars) and chars[read] == char:
+                count +=1
+                read +=1
+            
+            chars[write] = char
+            write += 1 
+
+            if count > 1:
+                for digit in str(count):
+                    chars[write] = digit
+                    write+= 1
+                
+
+        return write
         

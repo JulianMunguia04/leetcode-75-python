371c58219fb3ad7c59ddfe77f92ba3233a4ee8e8
Julian Munguia <julianmunguia04@gmail.com>
2026-07-29 21:25:13 +0900

    Update unique_number_of_occurances.py

--- Hashmap_Set/unique_number_of_occurances.py
+++ Hashmap_Set/unique_number_of_occurances.py
@@ -1,1 +1,9 @@
 # 1207 Unique Number of Occurances, Hashmap
+
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        

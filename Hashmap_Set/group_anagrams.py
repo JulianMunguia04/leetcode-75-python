47ce7c4336bea5efa2a76a061bc2f308d52f4e83
JulianMunguia04 <julianmunguia04@gmail.com>
2026-08-03 14:12:19 -0600

    Group anagrams

--- Hashmap_Set/group_anagrams.py
+++ Hashmap_Set/group_anagrams.py
@@ -1,1 +1,24 @@
 # 49 Group  Anagrams, Hashmap
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        
+        h = {}
+        l = []
+
+        for s in strs:
+            sorted_text = "".join(sorted(s))
+
+            if sorted_text in h:
+                h[sorted_text].append(s)
+            else:
+                h[sorted_text] = [s]
+
+        for value in h.values():
+            l.append(value)
+            
+        return l

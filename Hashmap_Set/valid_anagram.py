53757314d4af511e3c7759b910285798c4c92c9e
JulianMunguia04 <julianmunguia04@gmail.com>
2026-08-03 13:58:19 -0600

    Valid Anagram

--- Hashmap_Set/valid_anagram.py
+++ Hashmap_Set/valid_anagram.py
@@ -1,1 +1,29 @@
 # 242 Valid Anagram, Hashmap
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        h1 = {}
+        h2 = {}
+
+        if len(s) != len(t):
+          return False
+
+        for i in range(len(s)):
+          if s[i] in h1:
+            h1[s[i]] += 1
+          else: 
+            h1[s[i]] = 1
+          
+          if t[i] in h2:
+            h2[t[i]] += 1
+          else: 
+            h2[t[i]] = 1
+        
+        print(h1, h2)
+        return h1 == h2

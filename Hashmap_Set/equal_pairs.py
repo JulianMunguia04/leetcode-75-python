d93b256ee19364bec66cf230ba161d3682bead91
JulianMunguia04 <julianmunguia04@gmail.com>
2026-08-04 17:44:53 -0600

    equal pairs

--- Hashmap_Set/equal_pairs.py
+++ Hashmap_Set/equal_pairs.py
@@ -1,1 +1,24 @@
 # 2352 Equal Pairs, Hashmap
+
+class Solution(object):
+    def equalPairs(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        rows = {}
+
+        # Count each row
+        for row in grid:
+            row = tuple(row)
+            rows[row] = rows.get(row, 0) + 1
+
+        pairs = 0
+        n = len(grid)
+
+        # Check each column
+        for j in range(n):
+            col = tuple(grid[i][j] for i in range(n))
+            pairs += rows.get(col, 0)
+
+        return pairs

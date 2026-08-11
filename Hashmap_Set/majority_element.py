730e057b4ca8c1ba4c8d9d4b90985bf6367d0c09
JulianMunguia04 <julianmunguia04@gmail.com>
2026-08-11 00:24:33 -0600

    majority element

--- Hashmap_Set/majority_element.py
+++ Hashmap_Set/majority_element.py
@@ -1,1 +1,24 @@
 # 169 Majority Element, Hashmap, Easy
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+
+        hashmap = {}
+        highest = 0
+        largest = 0
+
+        for i in range(len(nums)):
+            if nums[i] not in hashmap:
+                hashmap[nums[i]] = 1
+            else: 
+                hashmap[nums[i]] += 1
+            if hashmap[nums[i]] > highest:
+                highest = hashmap[nums[i]]
+                largest = nums[i]
+
+        return largest

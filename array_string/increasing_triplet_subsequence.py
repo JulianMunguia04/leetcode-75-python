64e71e5cdb87431cd03ebd4cbcb67dfb66b778f0
JulianMunguia04 <julianmunguia04@gmail.com>
2026-08-28 22:42:40 -0600

    increasing triplet subsequence

--- array_string/increasing_triplet_subsequence.py
+++ array_string/increasing_triplet_subsequence.py
@@ -1,1 +1,21 @@
 # 334 Increasing Triplet Subsequence, Array / String
+
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        first = float('inf')
+        second = float('inf')
+
+        for num in nums:
+          if num <= first:
+            first = num
+          elif num <= second:
+            second = num
+          else:
+            return True
+        
+        return False

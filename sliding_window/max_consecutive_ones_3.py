8f0ee178ef2e436ff70f5b07a5ed19c56f33702d
JulianMunguia04 <julianmunguia04@gmail.com>
2026-09-12 00:48:20 -0600

    max consecutive ones

--- sliding_window/max_consecutive_ones_3.py
+++ sliding_window/max_consecutive_ones_3.py
@@ -1,1 +1,18 @@
 # 1004 Max Consecutive Ones III, Sliding Window
+
+def longestOnes(nums: list[int], k: int) -> int:
+    left = 0
+    max_length = 0
+    
+    for right in range(len(nums)):
+        if nums[right] == 0:
+            k -= 1
+            
+        while k < 0:
+            if nums[left] == 0:
+                k += 1
+            left += 1
+            
+        max_length = max(max_length, right - left + 1)
+        
+    return max_length

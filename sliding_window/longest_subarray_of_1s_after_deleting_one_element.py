1bbcdea24dab598d33d0e45322585026f513c039
JulianMunguia04 <julianmunguia04@gmail.com>
2026-09-12 00:53:42 -0600

    Solution

--- sliding_window/longest_subarray_of_1s_after_deleting_one_element.py
+++ sliding_window/longest_subarray_of_1s_after_deleting_one_element.py
@@ -1,1 +1,20 @@
 # 1493 Longest Subarray of 1's After Deleting One Element, Sliding Window, Medium
+
+class Solution:
+    def longestSubarray(self, nums: list[int]) -> int:
+        left = 0
+        zero_count = 0
+        max_length = 0
+        
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zero_count += 1
+                
+            while zero_count > 1:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left += 1
+            
+            max_length = max(max_length, right - left)
+            
+        return max_length

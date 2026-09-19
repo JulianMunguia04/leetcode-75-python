14200f5388aca290b014f3a06af91d45bd34428b
JulianMunguia04 <julianmunguia04@gmail.com>
2026-09-18 18:22:04 -0600

    Solution

--- binary-tree-DFS/leaf_similar_trees.py
+++ binary-tree-DFS/leaf_similar_trees.py
@@ -1,1 +1,36 @@
 # 872 Leaf-Similar Trees, Binary Tree DFS, Easy
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def leafSimilar(self, root1, root2):
+        """
+        :type root1: Optional[TreeNode]
+        :type root2: Optional[TreeNode]
+        :type root3: Optional[TreeNode]
+        :rtype: bool
+        """
+        # Helper function to collect leaves from left to right using Depth-First Search (DFS)
+        def get_leaves(root):
+            leaves = []
+            stack = [root]
+            while stack:
+                node = stack.pop()
+                if node:
+                    # If it's a leaf node, add its value to the list
+                    if not node.left and not node.right:
+                        leaves.append(node.val)
+                    # Push right first so left is processed first (LIFO order)
+                    if node.right:
+                        stack.append(node.right)
+                    if node.left:
+                        stack.append(node.left)
+            return leaves
+        
+        # Compare the leaf value sequences of both trees
+        return get_leaves(root1) == get_leaves(root2)
